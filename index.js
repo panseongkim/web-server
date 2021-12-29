751b2e585b1f13d1d761b42cbb7f20b6b83c476b
efforr77@docomo.ne.jp <panseong.kim@ibm.com>
2021-12-29 14:27:37 +0900

    HEROKU対応

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.post("/api/v1/quiz", function (req, res) {
   res.send(answer);
 });
 
-app.listen(3000, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
   console.log("Server is running");
 });
